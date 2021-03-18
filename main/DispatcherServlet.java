83b81031d517dc94559dd485ae3879fcbfd2e9ef
kanae2011 <62246184+kanae2011@users.noreply.github.com>
2021-03-18 17:22:32 +0900

    Update DispatcherServlet.java

--- main/DispatcherServlet.java
+++ main/DispatcherServlet.java
@@ -1,6 +1,9 @@
 package com.webjjang.main.controller;
 
 import java.io.IOException;
+import java.io.PrintWriter;
+import java.util.Date;
+
 import javax.servlet.ServletException;
 //import javax.servlet.annotation.WebServlet;
 import javax.servlet.http.HttpServlet;
@@ -35,6 +38,9 @@ public class DispatcherServlet extends HttpServlet {
 		// TODO Auto-generated method stub
 		// 이 곳에서 처리해야 할 모든 URL(*.do)을 받도록 설정 -> web.xml
 		System.out.println("DispatcherServelt.service()-*.do");
+		
+		//순수한 데이터를 전달하기 위한 객체
+		PrintWriter out = response.getWriter();
 
 		//String module = AuthorityFilter.url.substring(0, AuthorityFilter.url.indexOf("/", 1));
 		//System.out.println("url:" + AuthorityFilter.url);
@@ -44,6 +50,25 @@ public class DispatcherServlet extends HttpServlet {
 		//module이 존재하면 바꿈. "/main.do" : module이 존재하지 않음. module 변수게 있는 값은 바뀌지 않음
 		if(endIndex >= 0) module = AuthorityFilter.url.substring(0,endIndex);
 		System.out.println("DispatcherServelt.service().module:" + module);
+		
+		//모듈에 포함이 안되어 있는 URL의 처리 -> siteMesh에 적용이 안되도록 만들어야 하므로 
+		if(AuthorityFilter.url.equals("/ajax/checkId.do")) {
+			module = "/member"; //MemberController가 선택 
+		}
+		else if(AuthorityFilter.url.equals("/ajax/getMessageCnt.do")) {
+			module = "/message"; //MessageController가 선택 
+			//DB에서 데이터 가져오기 - 새로운 메세지의 갯수를 가져오는 프로그램 
+			//controller-service-dao
+			try {
+				String data = Beans.getController(module).execute(request);
+				System.out.println("DispatcherServelt.service().module:" + module + "::ajax:getMessageCnt");
+				out.write(data);
+			} catch (Exception e) {
+				// TODO Auto-generated catch block
+				e.printStackTrace();
+			}
+			return;
+		}
 
 		try {
 
